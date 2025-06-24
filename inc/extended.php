0248a1151e22e5982c851251c09974cfeeb9fd0c
Chano Vera <github@chanovera.com>
2025-06-24 09:44:24 -0600

    add a meta description on head

--- inc/extended.php
+++ inc/extended.php
@@ -58,3 +58,23 @@ function attachments_icons() {
     <?php
 }
 add_action('wp_head', 'attachments_icons');
+
+/**
+ * Meta description
+ */
+function get_dynamic_meta_description() {
+    if ( is_singular() ) {
+        global $post;
+
+        if ( has_excerpt( $post->ID ) ) {
+            return get_the_excerpt( $post->ID );
+        }
+
+        $custom_meta = get_post_meta( $post->ID, 'meta_description', true );
+        if ( $custom_meta ) {
+            return $custom_meta;
+        }
+    }
+
+    return get_bloginfo( 'description' );
+}
