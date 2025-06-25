9746799061b62d821b8da7d6d7a80f5f582704b3
Chano Vera <github@chanovera.com>
2025-06-24 19:44:04 -0600

    add styles for latest projects

--- inc/templates.php
+++ inc/templates.php
@@ -12,6 +12,7 @@ function frontpage_styles() {
         wp_enqueue_script( 'blur-typing', get_template_directory_uri() . '/assets/js/blur-typing.js', [], get_asset_version( '/assets/js/blur-typing.js' ), true );
         wp_enqueue_style( 'about-frontpage', get_template_directory_uri() . '/assets/css/frontpage/about.css', [], get_asset_version( '/assets/css/frontpage/about.css' ), 'all' );
         wp_enqueue_script( 'about-animation', get_template_directory_uri() . '/assets/js/about-animation.js', [], get_asset_version( '/assets/js/about-animation.js' ), true );
+        wp_enqueue_style( 'works-frontpage', get_template_directory_uri() . '/assets/css/frontpage/works.css', [], get_asset_version( '/assets/css/frontpage/works.css' ), 'all' );
         wp_enqueue_style( 'skills', get_template_directory_uri() . '/assets/css/frontpage/skills.css', [], get_asset_version( '/assets/css/frontpage/skills.css' ), 'all' );
         wp_enqueue_script( 'send-email', get_template_directory_uri() . '/assets/js/send-email.js', [], get_asset_version( '/assets/js/send-email.js' ), true );
 
