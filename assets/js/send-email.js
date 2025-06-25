391cb02345e7fbdce4d131824abcdf8fbb92d1a1
Chano Vera <github@chanovera.com>
2025-06-24 18:14:06 -0600

    add custom styles for hide and response on frontpage contact

--- assets/js/send-email.js
+++ assets/js/send-email.js
@@ -1,33 +1,42 @@
-const form = document.querySelector('.wpcf7-form')
-const showFormElement = document.getElementById('show-form')
-const response = document.querySelector('.wpcf7-response-output')
+const form = document.querySelector('.wpcf7-form');
+const formParagraphs = document.querySelectorAll('.wpcf7-form p');
+const showFormElement = document.getElementById('show-form');
+const response = document.querySelector('.wpcf7-response-output');
 
 function checkClassAndChangeStyles() {
     const titleElement = document.getElementById('title--get-in-touch');
-    form.classList.contains('submitting') | form.classList.contains('resetting') ? titleElement.classList.remove('hide') : form.classList.contains('sent') && (titleElement.classList.add('hide'),
-    showFormElement.classList.add('show'))
+    
+    if (form.classList.contains('submitting') || form.classList.contains('resetting')) {
+        titleElement.classList.remove('hide');
+    } else if (form.classList.contains('sent')) {
+        titleElement.classList.add('hide');
+        formParagraphs.forEach(p => p.classList.add('hide'));
+        showFormElement.classList.add('show');
+    }
 }
 
 const observerContact = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
-        mutation.type === 'attributes' && mutation.attributeName === 'class' && checkClassAndChangeStyles()
-    }
-    )
-}
-);
-
-form && observerContact.observe(form, {
-    attributes: true
+        if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+            checkClassAndChangeStyles();
+        }
+    });
 });
 
-checkClassAndChangeStyles();
+if (form) {
+    observerContact.observe(form, {
+        attributes: true
+    });
+    checkClassAndChangeStyles();
+}
+
 function showForm() {
-    const titleElement = document.getElementById('title--get-in-touch')
-      , showFormElement = document.getElementById('show-form');
-    titleElement.classList.remove('hide'),
-    showFormElement.classList.remove('show'),
-    form.classList.remove('sent'),
-    form.setAttribute('data-status', 'init')
+    const titleElement = document.getElementById('title--get-in-touch');
+    titleElement.classList.remove('hide');
+    showFormElement.classList.remove('show');
+    formParagraphs.forEach(p => p.classList.remove('hide'));
+    form.classList.remove('sent');
+    form.setAttribute('data-status', 'init');
 }
 
 if (showFormElement) {
