b9092357057ee0619bab502380c6151aea40741d
Chano Vera <github@chanovera.com>
2025-06-26 13:36:38 -0600

    update animation for about and latests works sections

--- assets/js/about-animation.js
+++ assets/js/about-animation.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const targets = document.querySelectorAll('#about .content, #works .content');
+  const targets = document.querySelectorAll('#about .content .title-section, #about .content .about__content p, #works .content .title-section, #works .content .card-timeline .card-item');
 
   if (!targets.length) return;
 
